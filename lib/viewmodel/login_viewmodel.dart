87690d96656d4d35f1a910743077f1680102a155
hossa <hossam.spiderman94@gmail.com>
2021-11-13 00:02:11 +0200

    LoginViewModel modifications

--- lib/viewmodel/login_viewmodel.dart
+++ lib/viewmodel/login_viewmodel.dart
@@ -6,25 +6,23 @@ class LoginViewModel {
   final firebaseIAuth _auth = firebaseIAuth().Singelton();
   //init your streams "LiveData"
   // ignore: unnecessary_new
-  //
-  //
+
   final BehaviorSubject<bool> _loadingIndicator =
       BehaviorSubject<bool>.seeded(false);
   final BehaviorSubject<bool> _isLoggedIn = BehaviorSubject<bool>.seeded(false);
-  //
-  //
-  //
+
   Stream<bool> get loadingIndicator => _loadingIndicator.stream;
   Stream<bool> get isLoggedIn => _isLoggedIn.stream;
-  //
-  //
+
   Future<bool> signInWithEmailAndPassword(String email, String password) async {
     _loadingIndicator.add(true);
     bool result = await _auth.signin(email, password);
     if (result) {
+      _isLoggedIn.add(true);
       _loadingIndicator.add(false);
       return true;
     } else {
+      _isLoggedIn.add(false);
       _loadingIndicator.add(false);
       return false;
     }
